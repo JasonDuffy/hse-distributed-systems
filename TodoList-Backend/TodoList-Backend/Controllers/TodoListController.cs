f23357f71c8c3ab28ff7db7c3c1029486698d38c
Tom Nguyen Dinh <n.tomhuy@gmail.com>
2024-05-12 11:10:25 +0200

    Database, Backend in Docker

--- TodoList-Backend/TodoList-Backend/Controllers/TodoListController.cs
+++ TodoList-Backend/TodoList-Backend/Controllers/TodoListController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoList_Backend.Model;
 using TodoList_Backend.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace TodoList_Backend.Controllers
 {
@@ -7,39 +9,78 @@ namespace TodoList_Backend.Controllers
     [Route("")]
     public class TodoListController : ControllerBase
     {
-        private readonly ILogger<TodoListController> _logger;
-        private readonly ITodoService _todoService;
+        /*
+                private readonly ILogger<TodoListController> _logger;
+                private readonly ITodoService _todoService;
 
-        public TodoListController(ILogger<TodoListController> logger, ITodoService todoService)
+                public TodoListController(ILogger<TodoListController> logger, ITodoService todoService)
+                {
+                    _logger = logger;
+                    _todoService = todoService;
+                }
+
+                [HttpGet("todos")]
+                [ProducesResponseType(typeof(string[]), StatusCodes.Status200OK)]
+                public IActionResult GetTodos()
+                {
+                    var todos = _todoService.GetTodos();
+                    _logger.LogInformation("Got todos: {todoList}", todos.ToList());
+                    return Ok(todos);
+                }
+
+                [HttpPost("todos/{todo}")]
+                [ProducesResponseType(StatusCodes.Status200OK)]
+                public IActionResult AddTodo([FromRoute] string todo)
+                {
+                    _logger.LogInformation("Adding todo: {todo}", todo);
+                    _todoService.AddTodo(todo);
+                    return Ok();
+                }
+
+                [HttpDelete("todos/{todo}")]
+                [ProducesResponseType(StatusCodes.Status200OK)]
+                public IActionResult DeleteTodo([FromRoute] string todo)
+                {
+                    _logger.LogInformation("Deleting todo: {todo}", todo);
+                    _todoService.DeleteTodo(todo);
+                    return Ok();
+                }
+           */
+
+        private readonly ApiDBContext _context;
+        public TodoListController(ApiDBContext apiDBContext)
         {
-            _logger = logger;
-            _todoService = todoService;
+            _context = apiDBContext;
         }
 
+
         [HttpGet("todos")]
-        [ProducesResponseType(typeof(string[]), StatusCodes.Status200OK)]
-        public IActionResult GetTodos()
+        public async Task<ActionResult<IEnumerable<string>>> GetTodoTitles()
         {
-            var todos = _todoService.GetTodos();
-            _logger.LogInformation("Got todos: {todoList}", todos.ToList());
-            return Ok(todos);
+            var todoTitles = await _context.Todos.Select(todo => todo.Title).ToListAsync();
+            return todoTitles;
         }
 
         [HttpPost("todos/{todo}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult AddTodo([FromRoute] string todo)
+        public IActionResult AddTodo([FromBody] Todo value)
         {
-            _logger.LogInformation("Adding todo: {todo}", todo);
-            _todoService.AddTodo(todo);
+            Model.Todo todo1 = new Model.Todo() { Title = value.Title };
+            _context.Todos.Add(todo1);
+            _context.SaveChanges();
+
             return Ok();
         }
 
         [HttpDelete("todos/{todo}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public IActionResult DeleteTodo([FromRoute] string todo)
+        public ActionResult<Todo> Delete(string todo)
         {
-            _logger.LogInformation("Deleting todo: {todo}", todo);
-            _todoService.DeleteTodo(todo);
+            var value = _context.Todos.FirstOrDefault(t => t.Title == todo);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            _context.Todos.Remove(value);
+            _context.SaveChanges();
             return Ok();
         }
     }
